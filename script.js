190f12e07733be05618866dac179feb507b006d2
JITENDRA KUMAR <jitendrakumar77800@gmail.com>
2025-03-17 00:03:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+var browserName = navigator.appName;
+    var version = navigator.appVersion;
+
+    var message = "You are using " + browserName + " version " + version;
+
+    var browserInfoDiv = document.getElementById("browser-info");
+    browserInfoDiv.innerHTML = message;
